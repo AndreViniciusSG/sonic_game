d45f6a77a81bf856c2b8e37fc2ff3138a553f916
AndreViniciusSG <andrevinicius_santos@hotmail.com>
2023-04-04 12:05:20 -0300

    Segunda versão do projeto
    Versão com parte de JavaScript concluída. Faltando detalhes no CSS e troca do obstáculo.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const sonic = document.querySelector('.sonic');
+const badnik = document.querySelector('.badnik');
 
 	const jump = () => {
 		sonic.classList.add('jump');
@@ -8,4 +9,25 @@ const sonic = document.querySelector('.sonic');
 		}, 600);
 	}
 	
+	const loop = setInterval(()=> {
+		
+		const badnikPosition = badnik.offsetLeft;
+		const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '');
+		
+		if(badnikPosition <=90 && badnikPosition > 0 && sonicPosition <50){
+			
+			badnik.style.animation = 'none';
+			badnik.style.left = `${badnikPosition}px`;
+			
+			sonic.style.animation = 'none';
+			sonic.style.bottom = `${sonicPosition}px`;
+			
+			sonic.src = 'images/gameover.png';
+			sonic.style.width = '100px';
+			
+			clearInterval(loop);
+			
+		}
+	}, 10);
+	alert("O Jogo será iniciado");
 	document.addEventListener('keydown', jump);
